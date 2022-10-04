5e0a1a15a520b70b073121f522791ff003463b82
MoYasser <mohamed.yasser2000@ymail.com>
2022-10-04 23:25:49 +0200

    added missing libs

--- launch/slambot.launch.py
+++ launch/slambot.launch.py
@@ -4,6 +4,8 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
