841c3a5d67a8d47d246dfae00cd7524609c27fa5
MoYasser <mohamed.yasser2000@ymail.com>
2022-10-04 23:34:45 +0200

    changed launch setup for rplidar

--- launch/slambot.launch.py
+++ launch/slambot.launch.py
@@ -15,58 +15,58 @@ def generate_launch_description():
     inverted = LaunchConfiguration('inverted', default='false')
     angle_compensate = LaunchConfiguration('angle_compensate', default='true')
 
-    # # Include the default rplidar_ros launch file with default parameters.
-    # # If you need to customize any rplidar parameters uncomment and use the
-    # # the launch config below.
-    # rplidar_launchDescription = IncludeLaunchDescription(
-        # PythonLaunchDescriptionSource(
-            # os.path.join(
-                # get_package_share_directory('rplidar_ros'),
-                # 'launch',
-                # 'rplidar.launch.py')),
-        # launch_arguments={}.items()
-    # )
+    # Include the default rplidar_ros launch file with default parameters.
+    # If you need to customize any rplidar parameters uncomment and use the
+    # the launch config below.
+    rplidar_launchDescription = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('rplidar_ros'),
+                'launch',
+                'rplidar.launch.py')),
+        launch_arguments={}.items()
+    )
 
     # This launch action (node) was copied from rplidar.launch.py
-    rplidar_launchDescription = ([
+    # rplidar_launchDescription = ([
 
-        DeclareLaunchArgument(
-            'serial_port',
-            default_value=serial_port,
-            description='Specifying usb port to connected lidar'),
+        # DeclareLaunchArgument(
+            # 'serial_port',
+            # default_value=serial_port,
+            # description='Specifying usb port to connected lidar'),
 
-        DeclareLaunchArgument(
-            'serial_baudrate',
-            default_value=serial_baudrate,
-            description='Specifying usb port baudrate to connected lidar'),
+        # DeclareLaunchArgument(
+            # 'serial_baudrate',
+            # default_value=serial_baudrate,
+            # description='Specifying usb port baudrate to connected lidar'),
 
-        DeclareLaunchArgument(
-            'frame_id',
-            default_value=frame_id,
-            description='Specifying frame_id of lidar'),
+        # DeclareLaunchArgument(
+            # 'frame_id',
+            # default_value=frame_id,
+            # description='Specifying frame_id of lidar'),
 
-        DeclareLaunchArgument(
-            'inverted',
-            default_value=inverted,
-            description='Specifying whether or not to invert scan data'),
+        # DeclareLaunchArgument(
+            # 'inverted',
+            # default_value=inverted,
+            # description='Specifying whether or not to invert scan data'),
 
-        DeclareLaunchArgument(
-            'angle_compensate',
-            default_value=angle_compensate,
-            description='Specifying whether or not to enable angle_compensate of scan data'),
+        # DeclareLaunchArgument(
+            # 'angle_compensate',
+            # default_value=angle_compensate,
+            # description='Specifying whether or not to enable angle_compensate of scan data'),
 
 
-        Node(
-            package='rplidar_ros2',
-            executable='rplidar_scan_publisher',
-            name='rplidar_scan_publisher',
-            parameters=[{'serial_port': serial_port, 
-                         'serial_baudrate': serial_baudrate, 
-                         'frame_id': frame_id,
-                         'inverted': inverted, 
-                         'angle_compensate': angle_compensate}],
-            output='screen'),
-    ])
+        # Node(
+            # package='rplidar_ros2',
+            # executable='rplidar_scan_publisher',
+            # name='rplidar_scan_publisher',
+            # parameters=[{'serial_port': serial_port, 
+                         # 'serial_baudrate': serial_baudrate, 
+                         # 'frame_id': frame_id,
+                         # 'inverted': inverted, 
+                         # 'angle_compensate': angle_compensate}],
+            # output='screen'),
+    # ])
 
     slambotaux_launchDescription = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
