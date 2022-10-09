10633c55cc2e7748e52cb657d4dcf415ca5758dd
MoYasser <mohamed.yasser2000@ymail.com>
2022-10-09 23:05:03 +0200

    changed from pakcage rplidar_ros2 to rplidar_ros

--- launch/slambot.launch.py
+++ launch/slambot.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
     rplidar_launchDescription = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(
-                get_package_share_directory('rplidar_ros2'),
+                get_package_share_directory('rplidar_ros'),
                 'launch',
                 'rplidar.launch.py')),
         launch_arguments={}.items()
